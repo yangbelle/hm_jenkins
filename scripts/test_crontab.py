c2f8ebceeb94859cf4f41b66fc8f4d104c84036a
yangbelle <337431259@qq.com>
2019-09-22 11:49:14 +0800

    测试crontab定时任务

---
+++ scripts/test_crontab.py
@@ -0,0 +1,16 @@
+import allure
+
+
+class TestCrontab:
+    """测试crontab定时任务"""
+    
+    @allure.step('前置条件')
+    def before(self):
+        print()
+        
+        
+    @allure.step('测试crontab定时任务')
+    def test_001(self):
+        self.before()
+        print('')
+	
